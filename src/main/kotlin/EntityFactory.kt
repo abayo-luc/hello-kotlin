e82a53f1c3bf9691156673a713eec820d1977101
AJL <luc.bayo@gmail.com>
2023-03-20 23:05:26 +0200

    topic: high order function

--- src/main/kotlin/EntityFactory.kt
+++ src/main/kotlin/EntityFactory.kt
@@ -27,13 +27,28 @@ sealed class Entity {
 }
 
 
-fun main(){
-    val message = when(EntityFactory.create(EntityType.EASY)){
-        is Entity.Easy -> "Easy class"
-        is Entity.Hard -> "Hard class"
-        Entity.Helper -> "Help class"
-        is Entity.Medium -> "Meidum class"
-    }
-    println(message)
+// adding property and method to a class you don't control
+fun Entity.Medium.printInfo(){
+    println("Medium class: $id")
+}
 
+val Entity.Medium.info: String
+    get() ="some info"
+fun main(){
+//    val message = when(EntityFactory.create(EntityType.EASY)){
+//        is Entity.Easy -> "Easy class"
+//        is Entity.Hard -> "Hard class"
+//        Entity.Helper -> "Help class"
+//        is Entity.Medium -> "Meidum class"
+//    }
+//    println(message)
+    val entity1 = Entity.Easy(UUID.randomUUID().toString(), "name")
+    val entity2 = EntityFactory.create(EntityType.MEDIUM)
+    val entities = arrayOf(entity1, entity2)
+    entities.forEach { entity ->
+        if(entity is Entity.Medium){
+            entity.printInfo()
+            println(entity.info)
+        }
+    }
 }
---
+++ src/main/kotlin/HigiherOrderFunction.kt
@@ -0,0 +1,33 @@
+// high order function are functions that either return another function or take functions as paramter values
+
+fun printFilteredString(list: List<String>, predicate:((String) -> Boolean)?){
+    list.forEach {
+        if(predicate?.invoke(it) === true){
+            println(it)
+        }
+    }
+}
+
+fun getPrintPredicate(): (String) -> Boolean {
+    return { it.startsWith("J")}
+}
+fun main(){
+    val list = listOf("Kitlin", "Java", "JavaScript", "C++")
+
+//    printFilteredString(list, {it.startsWith("K")})
+//
+//    // we can convert the above into lambda function to look like this:
+//    printFilteredString(list) {
+//        it.startsWith("K")
+//    }
+//
+//    printFilteredString(list, getPrintPredicate())
+
+    list.filterNotNull().filter{it.startsWith("J")}
+        .map{
+            it.length
+        }
+        .forEach{
+        println(it)
+    }
+}
